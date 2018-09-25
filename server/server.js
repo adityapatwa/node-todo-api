aafcd45883c03e67575c481a5706a48d5509b439
Aditya Patwa <adityapatwa@live.com>
2018-09-25 12:05:28 -0400

    Created the POST /todos API. Also refactored some of the code to seperate the connection and models into sperate files

--- server/server.js
+++ server/server.js
@@ -1,62 +1,25 @@
-let mongoose = require('mongoose');
+const express = require('express');
+const bodyParse = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-let Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express();
+app.use(bodyParse.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
 });
 
-// let newTodo = new Todo({
-//     text: 'Cook Dinner'
-// });
-//
-// newTodo.save().then((doc) => {
-//     console.log(`Saved todo`, doc);
-// }, (error) => {
-//     console.log(error);
-// });
-
-// let newTodo2 = new Todo({
-//     text: ' Feed the dog ',
-//     completed: true,
-//     completedAt: 2131
-// });
-
-// newTodo2.save().then((doc) => {
-//     console.log(`Saved todo`, doc);
-// }, (error) => {
-//     console.log('Unable to save object', error);
-// });
-
-let User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
-});
-
-let newUser = new User({
-    email: 'adityapatwa@email.com'
-});
-
-newUser.save().then((doc) => {
-    console.log(doc);
-}, (error) => {
-    console.log('Unable to create user', error);
+app.listen(3000, ()=> {
+   console.log(`Started on port 3000`);
 });
