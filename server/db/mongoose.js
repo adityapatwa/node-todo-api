235d27829e28d837b6a731fa10750a0ec7484fa5
Aditya Patwa <adityapatwa@live.com>
2018-09-25 13:47:13 -0400

    Added logs for debuggung

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+console.log("================================");
+console.log(process.env.MONGODB_URI);
+console.log("================================");
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 module.exports = {mongoose};
