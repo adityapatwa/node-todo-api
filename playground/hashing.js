4b7ac06c7c9d9f0f434d75aa67ea64f156efe42f
Aditya Patwa <adityapatwa@live.com>
2018-10-04 18:42:30 -0400

    Now passwords will be hashed before they are sotred in the database

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 9
-};
+let password = '123abcd!';
 
-let token = jwt.sign(data, '123abcd');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+   bcrypt.hash(password, salt, (err, hash) => {
+       console.log(hash);
+   })
+});
 
-let decoded = jwt.verify(token, '123abcd');
-console.log(decoded);
+let hashedPassword = '$2a$10$RVI4ITUhGODJqVObGVUNt.uGFHXFLYzS5nanE5LXUIZ4.WK32a3La'
+bcrypt.compare(password, hashedPassword, (err, res) => {
+
+});
+
+// let data = {
+//     id: 9
+// };
 //
+// let token = jwt.sign(data, '123abcd');
+// console.log(token);
+//
+// let decoded = jwt.verify(token, '123abcd');
+// console.log(decoded);
+// //
 // let message = 'I am user number 1';
 // let hash = SHA256(message).toString();
 //
