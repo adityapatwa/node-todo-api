9cfbc1e4a0b2e5a740c10f06ab5cec94bb9423a4
Aditya Patwa <adityapatwa@live.com>
2018-10-04 14:52:33 -0400

    Added the PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -74,12 +74,28 @@ app.delete('/todos/:id', (req, res) => {
 
 // Patch a todo
 app.patch('/todos/:id', (req, res) => {
-    let id = req.id.params;
+    let id = req.params.id;
     let body = _.pick(req.body, ['text', 'completed']);
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((error) => {
+        res.status(404).send();
+    });
 });
 
 app.listen(port, () => {
