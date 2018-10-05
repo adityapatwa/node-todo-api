e08d0a16561dc4c5fa76a3c4d75aae235590ebe7
Aditya Patwa <adityapatwa@live.com>
2018-10-05 14:07:15 -0400

    Removed all the deprecation warnings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
-let mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 module.exports = {mongoose};
